40bb2a0bb1d9f96ec42c2f33acfbf108a34546b9
Steel Brain <aneesiqbalbhatti@gmail.com>
2015-05-23 08:43:32 -0700

    :new: Add serialize and serializeAssoc

--- Mods.js
+++ Mods.js
@@ -1,5 +1,8 @@
 "use strict";
 
+
+NodeList.prototype.forEach = HTMLFormControlsCollection.prototype.forEach = Array.prototype.forEach;
+
 Function.prototype.memoize = function(){
   var Me = this;
   this.cache = {};
@@ -15,4 +18,25 @@ Node.prototype.closest = function(Match){
     if(el.match(Match)) return el;
   }
   return null;
+};
+
+HTMLFormElement.prototype.serialize = function(){
+  var ToReturn = [];
+  var Vals = this.serializeAssoc();
+  for(var i in Vals){
+    ToReturn.push(i + '=' + Vals[i]);
+  }
+  return ToReturn.join('&');
+};
+HTMLFormElement.prototype.serializeAssoc = function(){
+  var ToReturn = {};
+  var LFFix = /\r?\n/g;
+  var SpaceFix = /%20/g;
+  this.elements.forEach(function(n){
+    if (!n.name || ((n.type === 'checkbox' || n.type === 'radio') && !n.checked)){
+      return ;
+    }
+    ToReturn[n.name] = n.value.replace(LFFix, "\n").replace(SpaceFix, '+');
+  });
+  return ToReturn;
 };
