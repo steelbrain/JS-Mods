94d61f679a010d9eb6327e44886a8ef4c4167209
steelbrain <aneesiqbalbhatti@gmail.com>
2015-07-26 23:19:50 -0600

    :new: Add some more mods

--- mods/document.js
+++ mods/document.js
@@ -7,4 +7,17 @@ module.exports = function(Prototype){
   Prototype.Ready = function(Callback){
     this.readyState === "complete" || this.readyState === 'interactive' ? Callback() : this.on('DOMContentLoaded', Callback)
   }
+  Prototype.onScrollToBottom = function(Callback){
+    let InProgress = false
+    let Watcher = e => {
+      if (InProgress) return
+      if (this.body.scrollHeight <=
+        this.body.scrollTop +
+        window.innerHeight + 1000) {
+        InProgress = true
+        ;(new Promise(resolve => resolve(Callback(e)))).then(() => InProgress = false)
+      }
+    }
+    return this.on('scroll', Watcher)
+  }
 }
--- mods/misc.js
+++ mods/misc.js
@@ -1,4 +1,11 @@
 module.exports = function(){
   NodeList.prototype.forEach = HTMLCollection.prototype.forEach = HTMLFormControlsCollection.prototype.forEach = Array.prototype.forEach
   NodeList.prototype.indexOf = HTMLCollection.prototype.indexOf = HTMLFormControlsCollection.prototype.indexOf = Array.prototype.indexOf
+  Promise.prototype.finally = function(callback){
+    return this.then(result => callback(null, result), error => callback(error))
+  }
+  Location.prototype.getParam = function(key){
+    let value = new RegExp('[?&]'+key+'=[^&]+').exec(this.search)
+    return !!value ? decodeURIComponent(value.toString().replace(/^[^=]+./,'')) : false
+  }
 }
