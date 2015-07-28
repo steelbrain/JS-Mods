e819e342d4a4f6de99ae88e1d4a57907b0a0cf81
steelbrain <aneesiqbalbhatti@gmail.com>
2015-07-28 01:10:16 -0600

    :arrow_up: Bump version to 0.0.8

--- browser.js
+++ browser.js
@@ -211,8 +211,20 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     };
   }, {}], 7: [function (require, module, exports) {
     module.exports = function () {
+      Array.prototype.last = function () {
+        return this[this.length - 1];
+      };
+      Array.prototype.at = function (index) {
+        if (index >= 0) {
+          return this[index];
+        } else {
+          return this[this.length + index - 1];
+        }
+      };
       NodeList.prototype.forEach = HTMLCollection.prototype.forEach = HTMLFormControlsCollection.prototype.forEach = Array.prototype.forEach;
       NodeList.prototype.indexOf = HTMLCollection.prototype.indexOf = HTMLFormControlsCollection.prototype.indexOf = Array.prototype.indexOf;
+      NodeList.prototype.last = HTMLCollection.prototype.last = HTMLFormControlsCollection.prototype.last = Array.prototype.last;
+      NodeList.prototype.at = HTMLCollection.prototype.at = HTMLFormControlsCollection.prototype.at = Array.prototype.at;
       Promise.prototype["finally"] = function (callback) {
         return this.then(function (result) {
           return callback(null, result);
