0c2e6fa831a70cfbc34a32819654f5d63596b87c
steelbrain <aneesiqbalbhatti@gmail.com>
2015-07-28 01:04:24 -0600

    :new: Add Array.prototype.last

--- mods/misc.js
+++ mods/misc.js
@@ -1,6 +1,10 @@
 module.exports = function(){
+  Array.prototype.last = function(){
+    return this[this.length - 1]
+  }
   NodeList.prototype.forEach = HTMLCollection.prototype.forEach = HTMLFormControlsCollection.prototype.forEach = Array.prototype.forEach
   NodeList.prototype.indexOf = HTMLCollection.prototype.indexOf = HTMLFormControlsCollection.prototype.indexOf = Array.prototype.indexOf
+  NodeList.prototype.last = HTMLCollection.prototype.last = HTMLFormControlsCollection.prototype.last = Array.prototype.last
   Promise.prototype.finally = function(callback){
     return this.then(result => callback(null, result), error => callback(error))
   }
